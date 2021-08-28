4e2eeff0762bd62bfe4ab22ba849676a85b49964
whythem12 <87075228+whythem12@users.noreply.github.com>
2021-08-28 19:30:00 +0300

    Update db.go

--- db.go
+++ db.go
@@ -1,25 +1,24 @@
-package main
+package db
 
 import (
 	"encoding/json"
 	"fmt"
 	"os"
+	
 )
 
 func main() {
 	
 	db := Db{
 		path: "asdf.json",
+		backupPath: "backup.json",
 	}
-
-	
+	db.Write("qwe" , "flgg")	
 }
-
-
 type Db struct{
 	path string
-	things map[string]interface{}
-	
+	inputs map[string]interface{}
+	backupPath string
 }
 
 
@@ -49,4 +48,55 @@ func (db *Db) Check(path Db) (inf os.FileInfo, err error) {
 		inf, err = os.Stat(path.path)
 	}
 	return
+}
+
+
+
+func (db *Db) Write(key string, value interface{}) *Db {
+    if db.inputs == nil {
+        db.inputs = map[string]interface{}{}
+    }
+
+    db.inputs[key] = value
+
+    data, err := json.Marshal(db.inputs)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    err = os.WriteFile(db.path, data, 666)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    return db
+}
+func (db *Db) Get(key string) interface{} {
+	a := db.inputs[key]
+	fmt.Println(a)
+	return db.inputs[key]
+}
+func (db *Db) Backup(){
+	fmt.Println(db.inputs)
+	if db.inputs == nil {
+        db.inputs = map[string]interface{}{}
+    }
+    data, err := json.Marshal(db.inputs)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    err = os.WriteFile(db.backupPath, data, 666)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+}
+func (db *Db) GetAll() interface{} {
+	a := db.inputs
+	fmt.Println(a)
+	return db.inputs
 }
